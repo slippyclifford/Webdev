ef7474d5fff265e9836bfde8d4ef6e86b987c337
Hayes Geldmacher <hayes.g850@gmail.com>
2019-01-30 10:29:27 -0700

    Dr. Mario hated that

--- Adventure/game.js
+++ Adventure/game.js
@@ -1,37 +1,29 @@
-// C O M M E N T
-/* B I G 
-C O M M E N T
-*/
-
-
-
-//alert("Give me my beyblades");
-//confirm ("Do you wanna die my friend?");
-//prompt("I'm serious about my blades dude");
 Game();
-
 function Game(){
 alert("Dr.Mario's silly clinic");
 var playername = prompt("What's your tag man");
-alert("Welcome to my silly virus clinic!" + playername);
+alert("Welcome to my silly virus clinic" +              playername);
     
     Clinic();
     
     function Clinic(){
-        var clinic = prompt("You enter the clinic, nostrils flaring as you breathe in the shockingly sterile air. There is an air of unnerving hostility drifting through the room. The Dr. is lounging behind the counter, idly tossing pills into a glass jar. \n - Look around ").tolowercase();
+        var room = prompt("You enter the clinic, nostrils flaring as you breathe in the shockingly sterile air. There is an air of unnerving hostility drifting through the room. The Dr. is lounging behind the counter, idly tossing pills into a glass jar. \n - Look \n - approach the doctor").toLowerCase();
       
-        if(clinic == "look around" || "look"){
-           var glance = prompt("A quick glance around the room reveals little. A smooth, two-legged beast slumbers in the corner, dreaming of fruit. n\- touch beast n\- stop looking");
+       if(room == "look"){
+var look = prompt("A glance around reveals an two-legged beast slumbering in the corner, his  emerald gaze dreaming of fruit. \n - touch beast \n - stop look")
+ 
+if(look == "touch beast"){
+    alert("Yoshi was awakened by the intrusion on his dreams and reacted with force. /n He opened his great jurassic jaws and bit down on instinct. You are no more.")
+    Clinic();
+}
+}
+        else if(room == "approach the doctor"){
+            alert("As the doctor meets your eyes, you freeze. His icy blue pupils contain a seemingly infinite expanse of knowledge frozen over by a cruel indifference to life and death")
+            var room = prompt("The doc lifts his gloved hand to reveal a Bloodred pill between his thumb and pointer finger \n- take pill \n - refuse")
         }
-            else if(clinic == "stop looking" || "stop"){
-                Clinic();
-            }
-            else if(clinic = "touch beast || touch"){
-                
-            }
-        }
-        else if{
-            alert("Dr.Mario hated that");
-            Clinic();
-        }
-    } 
+    else{
+        alert("Dr.Mario hated that")
+        Clinic();
+    }
+    }
+}

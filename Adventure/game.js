f5c11adf86032f75eea07c1c9e6f0c9e72c574e4
Hayes Geldmacher <hayes.g850@gmail.com>
2019-02-07 10:30:55 -0700

    fix

--- Adventure/game.js
+++ Adventure/game.js
@@ -16,40 +16,37 @@ alert("Welcome to my silly virus clinic \n" +  playername);
     function Clinic(){
         var room = prompt("You enter the clinic, nostrils flaring as you breathe in the shockingly sterile air. There is an air of unnerving hostility drifting through the room. The Dr. is lounging behind the counter, idly tossing pills into a glass jar. \n - Look \n - approach the doctor").toLowerCase();
       
-      if(room == "approach"){
-          var approach = alert("As the doctor meets your eyes, you freeze. His icy blue pupils contain a seemingly infinite expanse of knowledge frozen over by a cruel indifference to life and death")
-           
-         
-          prompt("The doc lifts his gloved hand to reveal a Bloodred pill between his thumb and pointer finger \n- take pill \n - refuse").toLowerCase();
-}
-          
+switch(room){
+     case "look":
+         prompt("A glance around reveals an two-legged beast slumbering in the corner, his  emerald gaze dreaming of a cookie. \n - touch beast \n - stop look");
         
-      }
-          Clinic();
-      }
-        else if(room == "look"){
-            var look = prompt("A glance around reveals an two-legged beast slumbering in the corner, his  emerald gaze dreaming of a cookie. \n - touch beast \n - stop look").toLocaleLowerCase();
-        }
-        
-        
-    else{
-        alert("Dr.Mario hated that")
+    case "touch beast":
+         alert("Yoshi was awakened by the intrusion on his dreams and reacted with force. \n He opened his great jurassic jaws and bit down on instinct. You are no more.");
+         Clinic();
+         break;
+    case "stop look":
+        alert("You stop looking")
         Clinic();
-    }
+        break; 
+        
+        break;
+         
+     case "touch beast":
+         alert("Yoshi was awakened by the intrusion on his dreams and reacted with force. \n He opened his great jurassic jaws and bit down on instinct. You are no more.");
+         Clinic();
+         break;
+       
+    case "approach":
+        alert("As the doctor meets your eyes, you freeze. His icy blue pupils contain a seemingly infinite expanse of knowledge frozen over by a cruel indifference to life and death. He lifts his gloved hand to reveal a Bloodred pill between his thumb and pointer finger \n- take pill \n - refuse");
+        break;
+}
+        
+
+
+   
+        
+ }
     
     }
-    function street(){
-        var insidehouse = prompt(" enter door#1 enter door#2 ").toLocaleLowerCase(); 
-        
-        switch(insidehouse){
-            case " first door ":
-                alert("I <3 luigi ");
-                break;
-                
-            case " second door " :
-                alert("You will never the see the sun again") ;
-               
-                break;
-        }
-    }
-}
+    
+ 

98eb4802a937dd41984628b3f1ed9e1edf27e04b
Hayes Geldmacher <hayes.g850@gmail.com>
2019-02-11 10:11:38 -0700

    Dr.Mario is coming and hes cummung hard

--- Adventure/game.js
+++ Adventure/game.js
@@ -31,22 +31,28 @@ switch(room){
         
         break;
          
-     case "touch beast":
-         alert("Yoshi was awakened by the intrusion on his dreams and reacted with force. \n He opened his great jurassic jaws and bit down on instinct. You are no more.");
-         Clinic();
-         break;
        
     case "approach":
-        alert("As the doctor meets your eyes, you freeze. His icy blue pupils contain a seemingly infinite expanse of knowledge frozen over by a cruel indifference to life and death. He lifts his gloved hand to reveal a Bloodred pill between his thumb and pointer finger \n- take pill \n - refuse");
-        break;
-}
-        
+        prompt("As the doctor meets your eyes, you freeze. His icy blue pupils contain a seemingly infinite expanse of knowledge frozen over by a cruel indifference to life and death. He lifts his gloved hand to reveal a Bloodred pill between his thumb and pointer finger \n- take pill \n - refuse");
+    case "take pill":
+        alert("You cautiousy pluck the pill from the doc's rubber touch, raising it to your mouth as the sweet, salty taste of sweat trickles down your forhead. ")
+       var take = prompt("Are you sure you want to do this?")
 
-
+    if(take == "no"){
+        alert("You place the pill on your tounge, knowing full well that your doom is sealed as soon as your lips close.")
+    }
+       
+   if(take == "yes"){
+        alert("The second your pill hits the floor, you know that your doom is sealed. His eyes light up with a hellish rage as his mouth curls into a snarl so hideous god above would cower in fear.")
+   alert("RUN")
    
+       }
         
+        break;
+        break;
+      
  }
     
     }
-    
+   }
  
